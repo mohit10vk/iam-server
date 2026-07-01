c8d173178e324d290f876695864e5259254e8dde
Mohit <mohitvishavkarma7520@gmail.com>
2026-07-01 23:05:55 +0530

    Initial commit

---
+++ src/test/java/com/security/iam_server/IamServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.iam_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IamServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
